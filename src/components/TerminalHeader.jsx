f60fed9734349cb078cdaf5c2fa0c6d83b267519
warun7 <varun.g.ug23@plaksha.edu.in>
2024-08-09 15:49:21 +0530

    changed header color to white

--- src/components/TerminalHeader.jsx
+++ src/components/TerminalHeader.jsx
@@ -1,14 +1,20 @@
 function TerminalHeader() {
   return (
     <div className="flex justify-between items-center mb-4 border-b border-green-400 pb-2">
-      <div className="flex space-x-2 items-center">
+      <div className="text-white flex space-x-2 items-center">
         <i className="fa-solid fa-terminal"></i>
       </div>
-      <div className="flex justify-between items-center">~:zsh</div>
+      <div className="text-white flex justify-between items-center">~:zsh</div>
       <div>
-        <i className="fa-solid fa-chevron-down"></i>
-        <i className="fa-solid fa-chevron-up px-2"></i>
-        <i className="fa-solid fa-xmark"></i>
+        <button className="text-white">
+          <i className="fa-solid fa-chevron-down"></i>
+        </button>
+        <button className="text-white">
+          <i className="fa-solid fa-chevron-up px-2"></i>
+        </button>
+        <button className="text-white">
+          <i className="fa-solid fa-xmark"></i>
+        </button>
       </div>
     </div>
   );
