79e66eb1bc0db722c4cfcb73bff8b352f3f45ff0
warun7 <varun.g.ug23@plaksha.edu.in>
2024-08-26 22:13:38 +0530

    more responsiveness

--- src/components/TerminalHeader.jsx
+++ src/components/TerminalHeader.jsx
@@ -23,22 +23,31 @@ const TerminalHeader = () => {
   return (
     <div className="flex justify-between items-center mb-4 border-b border-green-400 pb-2">
       <div className="text-white flex space-x-2 items-center">
-        <i className="fa-solid fa-terminal"></i>
+        <i className="fa-solid fa-terminal text-lg md:text-xl sm:text-2xl"></i>
       </div>
       <div
         onClick={handleZshClick}
-        className="text-white flex justify-between items-center cursor-pointer"
+        className="text-white flex justify-between items-center cursor-pointer text-lg md:text-xl sm:text-2xl"
       >
         ~:zsh
       </div>
-      <div>
-        <button onClick={handleDownClick} className="text-white">
+      <div className="flex space-x-4">
+        <button
+          onClick={handleDownClick}
+          className="text-white text-lg md:text-xl sm:text-2xl"
+        >
           <i className="fa-solid fa-chevron-down"></i>
         </button>
-        <button onClick={handleUpClick} className="text-white">
+        <button
+          onClick={handleUpClick}
+          className="text-white text-lg md:text-xl sm:text-2xl"
+        >
           <i className="fa-solid fa-chevron-up px-2"></i>
         </button>
-        <button onClick={handleXmarkClick} className="text-white">
+        <button
+          onClick={handleXmarkClick}
+          className="text-white text-lg md:text-xl sm:text-2xl"
+        >
           <i className="fa-solid fa-xmark"></i>
         </button>
       </div>
